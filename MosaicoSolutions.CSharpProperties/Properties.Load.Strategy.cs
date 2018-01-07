eabdc654c53385bc0f9d5dcc0c75db6197f64395
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2018-01-07 03:17:10 -0200

    Refactoring.

--- MosaicoSolutions.CSharpProperties/Properties.Load.Strategy.cs
+++ MosaicoSolutions.CSharpProperties/Properties.Load.Strategy.cs
@@ -1,30 +1,48 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MosaicoSolutions.CSharpProperties
 {
-    public sealed partial class Properties : IProperties
+    public sealed partial class Properties
     {
-        public static IProperties LoadFromStrategy(PropertiesStrategy strategy, string content)
+        public static IProperties LoadFromStrategy(PropertiesStrategy strategy, string path)
+            => LoadFromStrategy(strategy, new StreamReader(path));
+        
+        public static IProperties LoadFromStrategy(PropertiesStrategy strategy, Stream stream)
+            => LoadFromStrategy(strategy, new StreamReader(stream));
+        
+        public static IProperties LoadFromStrategy(PropertiesStrategy strategy, TextReader reader)
         {
             if(strategy == null)
                 throw new ArgumentException(nameof(strategy));
 
-            if(content == null)
-                throw new ArgumentException(nameof(content));
-            
-            return strategy(content);
+            if(reader == null)
+                throw new ArgumentException(nameof(reader));
+
+            using(reader)
+                return strategy(reader.ReadToEnd());
         }
 
-        public static Task<IProperties> LoadFromStrategyAsync(PropertiesStrategy strategy, string content)
+        public static Task<IProperties> LoadFromStrategyAsync(PropertiesStrategy strategy, string path)
+            => LoadFromStrategyAsync(strategy, new StreamReader(path));
+        
+        public static Task<IProperties> LoadFromStrategyAsync(PropertiesStrategy strategy, Stream stream)
+            => LoadFromStrategyAsync(strategy, new StreamReader(stream));
+
+        public static Task<IProperties> LoadFromStrategyAsync(PropertiesStrategy strategy, TextReader reader)
         {
             if(strategy == null)
                 throw new ArgumentException(nameof(strategy));
 
-            if(content == null)
-                throw new ArgumentException(nameof(content));
+            if(reader == null)
+                throw new ArgumentException(nameof(reader));
 
-            return Task.Run(() => strategy(content));
+            return Task.Run(async () =>
+            {
+                using(reader)
+                    return strategy(await reader.ReadToEndAsync());
+            });
         }
     }
 }
