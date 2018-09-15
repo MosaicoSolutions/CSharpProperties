6e618fa30f4e7b1c7f57887bc7e3807cb8f91831
bmoura <bruno.xavier.moura@gmail.com>
2018-09-14 22:41:30 -0300

    moving StreamExtensions.

--- MosaicoSolutions.CSharpProperties/IO/StreamExtensions.cs
+++ MosaicoSolutions.CSharpProperties/IO/StreamExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MosaicoSolutions.CSharpProperties.Extensions
+namespace MosaicoSolutions.CSharpProperties.IO
 {
     public static class StreamExtensions
     {
